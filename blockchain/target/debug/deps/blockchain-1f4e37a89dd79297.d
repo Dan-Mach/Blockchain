7c9c49ca542c5aa953da882de1f48623e8f76e8f
Dan-Mach <danreech83@gmail.com>
2025-09-12 19:21:18 +0300

    blockchain

---
+++ blockchain/target/debug/deps/blockchain-1f4e37a89dd79297.d
@@ -0,0 +1,5 @@
+/home/dimar/Desktop/Blockchain/blockchain/target/debug/deps/blockchain-1f4e37a89dd79297.d: src/main.rs
+
+/home/dimar/Desktop/Blockchain/blockchain/target/debug/deps/libblockchain-1f4e37a89dd79297.rmeta: src/main.rs
+
+src/main.rs:
---
+++ blockchain/target/debug/deps/blockchain-8887e8ec980347f3.d
@@ -0,0 +1,5 @@
+/home/dimar/Desktop/Blockchain/blockchain/target/debug/deps/blockchain-8887e8ec980347f3.d: src/main.rs
+
+/home/dimar/Desktop/Blockchain/blockchain/target/debug/deps/libblockchain-8887e8ec980347f3.rmeta: src/main.rs
+
+src/main.rs:
