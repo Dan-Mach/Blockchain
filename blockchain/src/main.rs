3c69495f59bc4a6d45b9d2cac208cbf031f9f11a
Dan-Mach <danreech83@gmail.com>
2025-09-13 19:41:54 +0300

    changes

--- blockchain/src/main.rs
+++ blockchain/src/main.rs
@@ -1,4 +1,13 @@
+use std::collections::BTreeMap;
 mod balances;
 fn main () {
     println!("Blockchain module loaded successfully.");
+
+    let mut map = BTreeMap::new();
+    map.insert("Alice", 100);
+    assert_eq!(map.get(&"Alice"), Some(&100));
+    assert_eq!(map.get(&"Bob"), None);
+
+    let x = map.get("assssss");
+     
 }
